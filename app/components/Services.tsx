9bfe4401aab77fafa18d0e97ca813975fda31ffc
Mesha <mashhoodkhan887@gmail.com>
2026-08-06 12:28:23 +0400

    Render uploaded service icons

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link'
 import { getServices } from '../lib/db'
 
@@ -189,7 +190,11 @@ export default async function Services() {
                   desc={s.excerpt || s.description}
                   icon={
                     s.icon ? (
-                      <span className="text-2xl leading-none">{s.icon}</span>
+                      /^(?:data:image\/svg\+xml|https?:\/\/|\/)/i.test(s.icon) ? (
+                        <img className="h-7 w-7 object-contain" src={s.icon} alt="" aria-hidden="true" />
+                      ) : (
+                        <span className="text-2xl leading-none">{s.icon}</span>
+                      )
                     ) : (
                       <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
